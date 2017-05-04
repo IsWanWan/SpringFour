08daf6401ffe7b0a1f463866cd0c4057259e052f
绾绾 <739174884@qq.com>
2017-05-04 09:52:46 +0800

    生成二维码

--- src/main/java/com/wanwan/common/test/Test.java
+++ src/main/java/com/wanwan/common/test/Test.java
@@ -14,27 +14,27 @@ import redis.clients.jedis.Jedis;
 public class Test {
     private static final RedisClient redisClient = new RedisClient();
     public static void main(String args[]){
-//        Jedis jedis = new Jedis("localhost");
-
-        Jedis jedis = redisClient.getResource();
-
-//        System.out.println("Connection to server sucessfully");
-//        //设置 redis 字符串数据
-//        jedis.set("hhhh", "zhaoxiuling");
-//        // 获取存储的数据并输出
-//        System.out.println("Stored string in redis:: "+ jedis.get("hhhh"));
-        Admin admin = new Admin();
-        admin.setUsername("HAHA");
-        admin.setAddress("上海");
-//        Admin a  = new Admin();
-//        a.setUsername("AA");
-//        a.setAddress("ddd");
-
-
-        RedisClient.getResource().set("admin1".getBytes(), SerializeUtil.serialize(admin));
-        byte[] bg1 =  RedisClient.getResource().get("admin1".getBytes());
-        Admin adminOne = (Admin) SerializeUtil.unserialize(bg1);
-        System.out.println(adminOne.getUsername());
+////        Jedis jedis = new Jedis("localhost");
+//
+//        Jedis jedis = redisClient.getResource();
+//
+////        System.out.println("Connection to server sucessfully");
+////        //设置 redis 字符串数据
+////        jedis.set("hhhh", "zhaoxiuling");
+////        // 获取存储的数据并输出
+////        System.out.println("Stored string in redis:: "+ jedis.get("hhhh"));
+//        Admin admin = new Admin();
+//        admin.setUsername("HAHA");
+//        admin.setAddress("上海");
+////        Admin a  = new Admin();
+////        a.setUsername("AA");
+////        a.setAddress("ddd");
+//
+//
+//        RedisClient.getResource().set("admin1".getBytes(), SerializeUtil.serialize(admin));
+//        byte[] bg1 =  RedisClient.getResource().get("admin1".getBytes());
+//        Admin adminOne = (Admin) SerializeUtil.unserialize(bg1);
+//        System.out.println(adminOne.getUsername());
 
 
     }
